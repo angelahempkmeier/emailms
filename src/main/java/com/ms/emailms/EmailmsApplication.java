c37721af6350e45778eb637dff354b3b9371a15c
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-07 16:42:02 -0300

    initial commit

---
+++ src/main/java/com/ms/emailms/EmailmsApplication.java
@@ -0,0 +1,13 @@
+package com.ms.emailms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/emailms/EmailmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.emailms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
