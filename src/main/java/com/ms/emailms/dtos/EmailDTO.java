76ae872211f059dd133c515d76a37026b43bbf41
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-08 22:02:59 -0300

    feat(dtos): creating EmailDTO.

---
+++ src/main/java/com/ms/emailms/dtos/EmailDTO.java
@@ -0,0 +1,9 @@
+package com.ms.emailms.dtos;
+
+import java.util.UUID;
+
+public record EmailDTO(UUID userId,
+                       String emailTo,
+                       String subject,
+                       String text) {
+}
